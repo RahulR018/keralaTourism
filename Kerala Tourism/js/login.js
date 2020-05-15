69cb046f3509b5426065a9e38f63d7270bca1fe3
RahulR018 <rahulrmenon@live.com>
2020-05-15 16:55:23 +0530

    Rev 2 corrections made on validation forms

--- Kerala Tourism/js/login.js
+++ Kerala Tourism/js/login.js
@@ -1,31 +1,76 @@
-var email= document.getElementById('email');
-var pass=document.getElementById('pass');
+var input = document.getElementById('email');
+var pass = document.getElementById('pass');
+var confirm = document.getElementById('confirmpassword');
+var phone = document.getElementById('telephone');
+var emailAlert = document.getElementById('emailHelp');
+var passAlert = document.getElementById('passHelp');
 
-function validate(){
-    if(email.value==''|| pass.value==''){
-        document.getElementById('email').style.borderColor = 'red';
-        document.getElementById('email').style.borderWidth = '2px';
-        document.querySelector('input#email').classList.add('error');
-        document.querySelector('#email').addEventListener('focus', resete);
 
-        document.getElementById('pass').style.borderColor = 'red';
-        document.getElementById('pass').style.borderWidth = '2px';
-        document.querySelector('input#pass').classList.add('error');
-        document.querySelector('#pass').addEventListener('focus', resetp);
+
+input.addEventListener('input', email_validate);
+pass.addEventListener('input', pass_validate);
+
+
+input.addEventListener('focus', resete);
+pass.addEventListener('focus', resetp);
+
+
+
+function validate() {
+    if (email_validate() == true && pass_validate() == true) {
+        return true;
+    } else if (email_validate() == false && pass_validate() == false) {
+        input.classList.add('error');
+        pass.classList.add('error');
         return false;
-    }
-    else{
+    } else if (email_validate() == false) {
+        input.classList.add('error');
+        return false;
+    } else if (pass_validate() == false) {
+        pass.classList.add('error');
+        return false;
+    } else {
         return true;
     }
 }
 
+function pass_validate() {
+    if (pass.value == '') {
+        pass.placeholder = "This field cannot be empty";
+        return false;
+    } else if (pass.value.length < 8) {
+        return false;
+    } else if (/^(?=(.*[a-z])+)(?=(.*[A-Z])+)(?=(.*[0-9])+)(?!.*\s).{8,}$/.test(pass.value)) {
+        if (pass.value.length == 8) {
+            return false;
+        } else if (pass.value.length == 12) {
+            return true;
+        } else if (pass.value.length >= 15) {
+            return true;
+        }
+    } else {
+        return false;
+    }
+}
+function email_validate() {
+    if (input.value == '') {
+        input.placeholder = "This Field cannot be empty";
+        return false;
+    } else if (/^[\w._]+@[a-zA-Z]+\.[a-zA-Z]{2,3}(\.[a-z]{2,3})?$/.test(input.value)) {
+        emailAlert.innerText = "valid Email";
+        emailAlert.style.color = "green";
+        return true;
+    } else {
+        emailAlert.innerText = "Invalid input";
+        emailAlert.style.color = "red";
+        return false;
+    }
+}
+
+
 function resete() {
-    document.getElementById('email').style.borderColor = '';
-    document.getElementById('email').style.borderWidth = '0px';
     document.querySelector('input#email').classList.remove('error');
 }
 function resetp() {
-    document.getElementById('pass').style.borderColor = '';
-    document.getElementById('pass').style.borderWidth = '0px';
     document.querySelector('input#pass').classList.remove('error');
 }
--- Kerala Tourism/js/signup.js
+++ Kerala Tourism/js/signup.js
@@ -1,114 +1,144 @@
-var email = document.getElementById('email');
+var input = document.getElementById('email');
 var pass = document.getElementById('password');
-var reafirm = document.getElementById('confirmpassword');
-var tele = document.getElementById('telephone');
+var confirm = document.getElementById('confirmpassword');
+var phone = document.getElementById('telephone');
 var emailAlert = document.getElementById('emailHelp');
 var passAlert = document.getElementById('passHelp');
 var confirmAlert = document.getElementById('confpassHelp');
 var teleAlert = document.getElementById('teleHelp');
-var regexx = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/;
+var emaildata=[];
+var passdata=[];
 
 
+input.addEventListener('input', email_validate);
+pass.addEventListener('input', pass_validate);
+confirm.addEventListener('input', confirm_validate);
+phone.addEventListener('input', phone_validate);
 
+input.addEventListener('focus', resete);
+pass.addEventListener('focus', resetp);
+confirm.addEventListener('focus', resetconfp);
+phone.addEventListener('focus', resettele);
 
 
 function validate() {
-    if (email.value == '' || pass.value == '' || reafirm.value == '' || tele.value == '') {
-        emailAlert.innerText = 'This field cannot be empty';
-        document.getElementById('email').style.borderColor = 'red';
-        document.getElementById('email').style.borderWidth = '2px';
-        document.querySelector('input#email').classList.add('error');
-        document.querySelector('#email').addEventListener('focus', resete);
-        passAlert.innerText = 'This field cannot be empty';
-        document.getElementById('password').style.borderColor = 'red';
-        document.getElementById('password').style.borderWidth = '2px';
-        document.querySelector('input#password').classList.add('error');
-        document.querySelector('#password').addEventListener('focus', resetp);
-        confirmAlert.innerText = 'This field cannot be empty';
-        document.getElementById('confirmpassword').style.borderColor = 'red';
-        document.getElementById('confirmpassword').style.borderWidth = '2px';
-        document.querySelector('input#confirmpassword').classList.add('error');
-        document.querySelector('#confirmpassword').addEventListener('focus', resetconfp);
-        teleAlert.innerText = 'This field cannot be empty';
-        document.getElementById('telephone').style.borderColor = 'red';
-        document.getElementById('telephone').style.borderWidth = '2px';
-        document.querySelector('input#telephone').classList.add('error');
-        document.querySelector('#telephone').addEventListener('focus', resettele);
+    if (email_validate() == true && pass_validate() == true && confirm_validate() == true &&
+        phone_validate() == true) {
+        return true;
+    } else if (email_validate() == false && pass_validate() == false && confirm_validate() == false &&
+        phone_validate() == false) {
+        input.classList.add('error');
+        pass.classList.add('error');
+        confirm.classList.add('error');
+        phone.classList.add('error');
+        return false;
+    } else if (email_validate() == false) {
+        input.classList.add('error');
+        return false;
+    } else if (pass_validate() == false) {
+        pass.classList.add('error');
+        return false;
+    } else if (confirm_validate() == false) {
+        confirm.classList.add('error');
+        return false;
+    } else if (phone_validate() == false) {
+        phone.classList.add('error');
+        return false;
+    } else {
+        return true;
+    }
+}
+
+function phone_validate() {
+    if (phone.value == '') {
+        phone.placeholder = "This field cannot be empty";
+        return false;
+    } else if (phone.value.length < 10) {
+        teleAlert.innerText = "Invalid Phone Number";
+        teleAlert.style.color = 'red';
+        return false;
+    } else if (phone.value.length == 10 && /^\d+$/.test(phone.value)) {
+        teleAlert.innerText = "Format is XXX-XXX-XXXX.(space or . also allowed in place of -)";
+        teleAlert.style.color = 'red';
+        return false;
+    } else if (/^[\d]{3}[\.\-\s][\d]{3}[\.\-\s][\d]{4}$/.test(phone.value)) {
+        teleAlert.innerText = "Number is valid";
+        teleAlert.style.color = "green";
+        return true;
+    } else {
+        teleAlert.innerText = 'Invalid Phone Number';
+        teleAlert.style.color = 'red';
         return false;
     }
-    else if (pass.value.length != 0) {
-        if (pass.value.search(/\d/) == -1) {
-            passAlert.innerText = 'Password need atleast a number';
-            return false;
-        }
-        else if (pass.value.search(/[A-Za-z]/) == -1) {
-            passAlert.innerText = 'Password must contain atleast 1 capital letter and a small letter';
-            return false;
-        }
-        else if (pass.value.length < 8) {
-            passAlert.innerText = 'password required minimum 8 characters!!';
+}
+function confirm_validate() {
+    if (pass.value == confirm.value && confirm.value.length != 0) {
+        confirmAlert.innerText = "perfect Match!";
+        confirmAlert.style.color = "green";
+        return true;
+    } else {
+        confirmAlert.innerText = "No match";
+        confirmAlert.style.color - "red";
+        return false;
+    }
+}
+function pass_validate() {
+    if (pass.value == '') {
+        pass.placeholder = "This field cannot be empty";
+        return false;
+    } else if (pass.value.length < 8) {
+        passAlert.innerText = "Minimum charaters is 8";
+        return false;
+    } else if (/^(?=(.*[a-z])+)(?=(.*[A-Z])+)(?=(.*[0-9])+)(?!.*\s).{8,}$/.test(pass.value)) {
+        if (pass.value.length == 8) {
+            passAlert.innerText = 'Poor Password';
             passAlert.style.color = 'red';
             return false;
-        }
-        else if (pass.value.length == 8) {
-            passAlert.innerText = 'Weak Password';
-            passAlert.style.color = 'red';
-            return false;
-        }
-        else if (pass.value.length <= 15) {
-            passAlert.innerText = 'Medium Password';
-            passAlert.style.color = 'orange';
-        }
-        else if (pass.value.length > 15) {
-            passAlert.innerText = 'Strong Password';
+        } else if (pass.value.length == 12) {
+            passdata.push(pass.value);
+            passAlert.innerText = "Medium Password";
+            passAlert.style.color = "orange";
+            return true;
+        } else if (pass.value.length >= 15) {
+            passAlert.innerText = "Strong Password";
             passAlert.style.color = 'green';
             return true;
         }
-        else if (pass.value != reafirm.value) {
-            confirmAlert.innerText = 'Passwords do not match';
-            return false;
-        }
-    }
-    else if (tele.value.length > 0) {
-        phonenumber(tele);
-    }
-    else {
-        return true;
-    }
-}
-
-
-
-function resete() {
-    document.getElementById('email').style.borderColor = '';
-    document.getElementById('email').style.borderWidth = '0px';
-    document.querySelector('input#email').classList.remove('error');
-}
-function resetp() {
-    document.getElementById('password').style.borderColor = '';
-    document.getElementById('password').style.borderWidth = '0px';
-    document.querySelector('input#password').classList.remove('error');
-}
-function resetconfp() {
-    document.getElementById('confirmpassword').style.borderColor = '';
-    document.getElementById('confirmpassword').style.borderWidth = '0px';
-    document.querySelector('input#confirmpassword').classList.remove('error');
-}
-function resettele() {
-    document.getElementById('telephone').style.borderColor = '';
-    document.getElementById('telephone').style.borderWidth = '0px';
-    document.querySelector('input#telephone').classList.remove('error');
-}
-
-function phonenumber(tele) {
-    var phoneno = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
-    if (inputtxt.value.match(phoneno)){
-        return true;
-    }
-    else {
-        alert("message");
+    } else {
+        passAlert.innerText = "Must have atleast 1 digit,1 Uppercase and 1 lowerCase letter";
+        passAlert.style.color = 'red';
         return false;
     }
 }
+function email_validate() {
+    if (input.value == '') {
+        input.placeholder = "This Field cannot be empty";
+        return false;
+    } else if (/^[\w._]+@[a-zA-Z]+\.[a-zA-Z]{2,3}(\.[a-z]{2,3})?$/.test(input.value)) {
+        emaildata.push(input.value);
+        emailAlert.innerText = "valid Email";
+        emailAlert.style.color = "green";
+        return true;
+    } else {
+        emailAlert.innerText = "Invalid input";
+        emailAlert.style.color = "red";
+        return false;
+    }
+}
+
+
+function resete() {
+    document.querySelector('input#email').classList.remove('error');
+}
+function resetp() {
+    document.querySelector('input#password').classList.remove('error');
+}
+function resetconfp() {
+    document.querySelector('input#confirmpassword').classList.remove('error');
+}
+function resettele() {
+    document.querySelector('input#telephone').classList.remove('error');
+}
+
 
 
